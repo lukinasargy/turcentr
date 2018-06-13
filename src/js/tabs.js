b457d38dfa78a3152994b47889372ba6e7f772af
Sargy Lukina <lukina_sargy@mail.ru>
2018-06-13 23:35:51 +0300

    tabs js

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -7,10 +7,22 @@ $(document).ready(function(){
       var activePanel = $("#"+activeId);
       var othersPanel = activePanel.siblings();
       othersPanel.hide();
-      activePanel.addClass("d_active").fadeIn();
+      activePanel.addClass("d_active").fadeIn().siblings().removeClass("d_active");
     });
   $(".tab-delete").on("click", function(){
     $(".tabs").find('.active').removeClass("active").hide().next(".tabs li").addClass("active").fadeIn();
     $(".d_active").removeClass("d_active").hide().next(".tab_content").addClass("d_active").fadeIn();
+  });
+  $(".tab-add").on("click", function(){
+    $(".tabs li").parent().append('<li>New tab</li>');
+    $(".tabs li").attr('rel',function(index, currentClassName){
+      if (!currentClassName) { 
+      return 'tab'+(index+1);
+      }    
+    });var newId=$(this).attr("rel")
+    var newPanel = $("#"+newId)
+    $(".tab_container").append('<div class="tab_content">New tab</div>').last().attr('id',newPanel);
+
+
   });
 });
